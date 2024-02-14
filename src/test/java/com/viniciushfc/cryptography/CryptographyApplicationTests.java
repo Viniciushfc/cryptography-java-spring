4d7ff3f26ae4e94e680d7e4ea3c528679a1a4680
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-14 18:38:12 -0300

    project default spring

---
+++ src/test/java/com/viniciushfc/cryptography/CryptographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciushfc.cryptography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
