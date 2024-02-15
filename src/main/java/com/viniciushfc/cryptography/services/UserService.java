95a45fdf140170ff925e4eac62e7d815684a43b2
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-14 22:32:10 -0300

    Service de User criado e iniciado as regras de negócio CRUD.

---
+++ src/main/java/com/viniciushfc/cryptography/services/UserService.java
@@ -0,0 +1,24 @@
+package com.viniciushfc.cryptography.services;
+
+import com.viniciushfc.cryptography.dtos.UserDTO;
+import com.viniciushfc.cryptography.entities.User;
+import com.viniciushfc.cryptography.infra.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO dto) {
+        User newUser = new User(dto);
+        saveUser(newUser);
+        return newUser;
+    }
+}
