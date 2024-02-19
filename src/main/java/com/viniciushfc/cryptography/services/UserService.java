de7cd41c1fbd3eb8deab3b1a87461babdbd26003
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-19 19:38:49 -0300

    Implementado parte de criptografia na service.

--- src/main/java/com/viniciushfc/cryptography/services/UserService.java
+++ src/main/java/com/viniciushfc/cryptography/services/UserService.java
@@ -6,6 +6,7 @@ import com.viniciushfc.cryptography.infra.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.List;
 
 @Service
@@ -14,12 +15,15 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private CryptographyService cryptographyService;
+
     public void saveUser(User user) {
         this.repository.save(user);
     }
 
-    public User createUser(UserDTO dto) {
-        User newUser = new User(dto);
+    public User createUser(UserDTO dto) throws Exception {
+        User newUser = cryptographyService.condifyUser(dto);
         saveUser(newUser);
         return newUser;
     }
