23cc1432a67543709c14f184e97784b518d260a6
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-19 19:36:58 -0300

    Trocado nome da variavel value para amount.

--- src/main/java/com/viniciushfc/cryptography/dtos/UserDTO.java
+++ src/main/java/com/viniciushfc/cryptography/dtos/UserDTO.java
@@ -1,4 +1,4 @@
 package com.viniciushfc.cryptography.dtos;
 
-public record UserDTO(String userDocument, String creditCardToken, Long value) {
+public record UserDTO(String userDocument, String creditCardToken, Long amount) {
 }
