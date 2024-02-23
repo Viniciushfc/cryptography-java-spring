b52c3cd008495c05937b82a8c9a113377361ccff
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-22 22:26:36 -0300

    Exceção Personalizada para NotFoundData.

---
+++ src/main/java/com/viniciushfc/cryptography/infra/exception/NotFoundData.java
@@ -0,0 +1,9 @@
+package com.viniciushfc.cryptography.infra.exception;
+
+public class NotFoundData extends RuntimeException  {
+
+    public NotFoundData(String mensagem) {
+        super(mensagem);
+
+    }
+}
